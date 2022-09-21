b1128ca1b4ab17fa7318ddd47b6acd9a39e3366a
Thibaud Marin <thibaud.marin1@gmail.com>
2022-09-21 22:46:44 +0200

    portfolio

---
+++ public/assets/js/script.js
@@ -0,0 +1,59 @@
+const navigation = document.querySelector('nav');
+const titreSpans = document.querySelectorAll('.titleSpans span')
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >930) {
+        navigation.classList.add('bgNav')
+    } else {
+        navigation.classList.remove('bgNav')
+    }
+})
+
+// anim click
+
+window.addEventListener('click', (e) =>{
+    const rond = document.createElement('div');
+    rond.className ='clickAnim';
+    rond.style.top =`${e.pageY - 50}px`;
+    rond.style.left = `${e.pageX -50}px`;
+    document.body.appendChild(rond);
+
+    setTimeout(()=> {
+    rond.remove();
+    },1500)
+}
+)
+// anim load title
+window.addEventListener ('load', () =>{
+    
+    const TL = gsap.timeline({pause:true});
+
+    TL
+    .staggerFrom(titreSpans, 1, { opacity: 0, ease: "power2.out"}, 0.3)
+
+    TL.play();
+})
+
+// apparition bloc en js 
+const ratio = .1
+const option ={
+    root:null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function(entries, observer){
+    entries.forEach(function(entry){
+        console.log(entry.intersectionRatio)
+        if (entry.intersectionRatio > ratio){
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+var observer = new IntersectionObserver(handleIntersect, option)
+document.querySelectorAll('.reveal').forEach(function (r){
+    observer.observe(r)
+})
+
